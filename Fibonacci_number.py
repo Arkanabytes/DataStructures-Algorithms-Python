63dc14cb6a86739774daa99cfd27aaa2fdd7966b
Goretti Rivera <gorettirivera@Gorettis-MacBook-Pro.local>
2020-04-17 17:28:34 -0700

    fibo

---
+++ Fibonacci_number.py
@@ -0,0 +1,28 @@
+
+#Memoization - idea: cache values
+cache = {}
+
+def fibonacci(n):
+    # Check that input is a positive integer.
+    if type(n) != int:
+        raise TypeError("n must be a positive int")
+    if n < 1:
+        raise ValueError("n must be a positive int")
+
+    # If we have cached the value, then return it.
+    if n in cache:
+        return cache[n]
+    
+    # Compute the Nth term 
+    if n <= 2:
+        value = 1 
+    else: 
+        value = fibonacci(n-1) + fibonacci(n-2)
+
+    # Cache the value and return it.
+    cache[n] = value
+    return value
+
+# The first 21 Fibonacci numbers Fn are
+for n in range(1,6):
+    print(n ,":" , fibonacci(n))
