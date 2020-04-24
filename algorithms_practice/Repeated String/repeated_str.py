7cc309161b855ad4b32dc0369d3d2e405860abaa
Goretti Rivera <gorettirivera@Gorettis-MacBook-Pro.local>
2020-04-23 21:44:33 -0700

    repeated_str

---
+++ algorithms_practice/Repeated String/repeated_str.py
@@ -0,0 +1,23 @@
+
+
+def repeatedString(s, n):
+    lengh = len(s)
+    extraCharm = n % lengh
+    num_a = 0
+    num_a_extra = 0
+    result = 0
+
+    for j,i in enumerate(s):
+        if i == "a":
+            num_a += 1
+        if i == "a" and j < extraCharm:
+            num_a_extra +=1
+        
+    
+    result = ((n // lengh ) * num_a) + num_a_extra
+    return result
+
+
+s = "aba"
+n = 10
+print(repeatedString(s,n))
